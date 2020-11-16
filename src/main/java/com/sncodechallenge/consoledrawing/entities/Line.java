796a9c0c720b17a14707443d8d63e3c656e34354
Inês <ines.calcoa@gmail.com>
2020-11-16 18:01:26 +0000

    Task_5 - Clean up Geometric Entities

--- src/main/java/com/sncodechallenge/consoledrawing/entities/Line.java
+++ src/main/java/com/sncodechallenge/consoledrawing/entities/Line.java
@@ -16,8 +16,7 @@ public class Line implements GeometricEntity {
 
         if (x1 != x2 && y1 != y2) {
             throw new IllegalArgumentException(
-                    (x1 + "," + x2 + "," + "," + y1 + "," + y2) + " - Diagonal lines are not supported");
-        }
+                    (x1 + "," + x2 + "," + "," + y1 + "," + y2) + " - Diagonal lines are not supported"); }
 
         int[] coordinatesArray = new int[]{x1, x2, y1, y2};
 
@@ -29,37 +28,17 @@ public class Line implements GeometricEntity {
         this.y2 = y2;
     }
 
-    public int getX1() {
-        return x1;
-    }
+    public int getX1() { return x1; }
 
-    public void setX1(int x1) {
-        this.x1 = x1;
-    }
 
-    public int getX2() {
-        return x2;
-    }
+    public int getX2() { return x2; }
 
-    public void setX2(int x2) {
-        this.x2 = x2;
-    }
 
-    public int getY1() {
-        return y1;
-    }
+    public int getY1() { return y1; }
 
-    public void setY1(int y1) {
-        this.y1 = y1;
-    }
 
-    public int getY2() {
-        return y2;
-    }
+    public int getY2() { return y2; }
 
-    public void setY2(int y2) {
-        this.y2 = y2;
-    }
 
     @Override
     public boolean equals(Object o) {
